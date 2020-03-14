cbc33cb55492cd12404b9beab847ba077ea0e0a5
bryan aviles <easyrun32@gmail.com>
2020-03-14 07:03:36 -0400

    removing captcha

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,17 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import Layout from '../layout'
+import React from "react";
+import { Link } from "gatsby";
+import Layout from "../layout";
 export default function Index() {
   return (
     <Layout>
       <h1>Hi people</h1>
       <p>
-        This is an example site integrating Netlify’s form handling with Gatsby. You can{' '}
-        <a href="https://github.com/sw-yx/gatsby-netlify-form-example-v2">view source here</a>.
+        This is an example site integrating Netlify’s form handling with Gatsby.
+        You can{" "}
+        <a href="https://github.com/sw-yx/gatsby-netlify-form-example-v2">
+          view source here
+        </a>
+        .
       </p>
       <ul>
         <li>
@@ -17,13 +21,13 @@ export default function Index() {
           <Link to="/file-upload/">Form with file upload</Link>
         </li>
         <li>
-          <Link to="/multipart-file-upload/">Form with multipart file upload</Link>
+          <Link to="/multipart-file-upload/">
+            Form with multipart file upload
+          </Link>
         </li>
+
         <li>
-          <Link to="/recaptcha/">Form with reCAPTCHA 2</Link>
-        </li>
-        <li>
-          You can also{' '}
+          You can also{" "}
           <a href="https://github.com/snipcart/gatsby-netlify-serverless/blob/master/functions/fetchreviews.js#L23">
             programmatically retrieve data
           </a>
@@ -31,5 +35,5 @@ export default function Index() {
         </li>
       </ul>
     </Layout>
-  )
+  );
 }
