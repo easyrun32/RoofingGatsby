ffb29ca145afc5e5c92e40686a6f936e097c4297
bryan aviles <easyrun32@gmail.com>
2020-03-20 15:31:08 -0400

    window

--- src/components/homepage-section/homepage-section.component.jsx
+++ src/components/homepage-section/homepage-section.component.jsx
@@ -15,89 +15,76 @@ import schedule from "../../images/circle-images/schedule.png";
 import roofcolors from "../../images/circle-images/roofcolors.jpg";
 import roofer from "../../images/circle-images/roofer.jpg";
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useLayoutEffect } from "react";
 // https://www.react-reveal.com/examples/
 
-function getWindowDimensions() {
-  if (typeof window !== "undefined") {
-    const { innerWidth: width, innerHeight: height } = window;
-    return {
-      width,
-      height
-    };
-  } else {
-    return null;
-  }
+// function getWindowDimensions() {
+//   if (typeof window !== "undefined") {
+//     const { innerWidth: width, innerHeight: height } = window;
+//     return {
+//       width,
+//       height
+//     };
+//   } else {
+//     return null;
+//   }
+// }
+function useWindowSize(defaultValue) {
+  const [windowSize, setWindowSize] = useState({ innerWidth: defaultValue });
+
+  useLayoutEffect(() => {
+    setWindowSize({ innerWidth: window.innerWidth });
+  }, []);
+
+  return windowSize;
 }
 export const HomepageSection = () => {
-  const [windowDimensions, setWindowDimensions] = useState(
-    getWindowDimensions()
-  );
+  // const [windowDimensions, setWindowDimensions] = useState(
+  //   getWindowDimensions()
+  // );
 
-  useEffect(() => {
-    function handleResize() {
-      setWindowDimensions(getWindowDimensions());
-    }
+  // useEffect(() => {
+  //   function handleResize() {
+  //     setWindowDimensions(getWindowDimensions());
+  //   }
 
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-  const { width } = windowDimensions;
+  //   window.addEventListener("resize", handleResize);
+  //   return () => window.removeEventListener("resize", handleResize);
+  // }, []);
+  // const { width } = windowDimensions;
+  const windowSize = useWindowSize(900);
+  console.log(windowSize);
   return (
     <Background>
       <br />
       <TextTitle>THE FRANK STEVENS & SONS EXPERIENCE</TextTitle>
-      {width < 800 ? (
-        <Fade bottom>
-          <Container>
-            <CircleComponent image={schedule} />
-            <h2>Schedule</h2>
-            <Text>We'll bring a roofer with 25 years of experience</Text>
-          </Container>
+      <Fade bottom>
+        <Grid>
+          <Left>
+            <Container>
+              <CircleComponent image={schedule} />
+              <h2>Schedule</h2>
+              <Text>We'll bring a roofer with 25 years of experience</Text>
+            </Container>
+          </Left>
+          <Middle>
+            <Container>
+              <CircleComponent image={roofcolors} />
+              <h2>Pick Material Color</h2>
+              <Text>Pick one of our beautiful colors</Text>
+            </Container>
+          </Middle>
+          <Right>
+            <Container>
+              <CircleComponent image={roofer} />
+              <h2>Install New Roof</h2>
+              <Text>We use Quality products </Text>
+            </Container>
+          </Right>
+        </Grid>
 
-          <Container>
-            <CircleComponent image={roofcolors} />
-            <h2>Pick Material Color</h2>
-            <Text>Pick one of our beautiful colors</Text>
-          </Container>
-
-          <Container>
-            <CircleComponent image={roofer} />
-            <h2>Install New Roof</h2>
-            <Text>We use Quality products </Text>
-          </Container>
-
-          <Button>Learn More</Button>
-        </Fade>
-      ) : (
-        <Fade bottom>
-          <Grid>
-            <Left>
-              <Container>
-                <CircleComponent image={schedule} />
-                <h2>Schedule</h2>
-                <Text>We'll bring a roofer with 25 years of experience</Text>
-              </Container>
-            </Left>
-            <Middle>
-              <Container>
-                <CircleComponent image={roofcolors} />
-                <h2>Pick Material Color</h2>
-                <Text>Pick one of our beautiful colors</Text>
-              </Container>
-            </Middle>
-            <Right>
-              <Container>
-                <CircleComponent image={roofer} />
-                <h2>Install New Roof</h2>
-                <Text>We use Quality products </Text>
-              </Container>
-            </Right>
-          </Grid>
-
-          <Button>Learn More</Button>
-        </Fade>
-      )}
+        <Button>Learn More</Button>
+      </Fade>
     </Background>
   );
 };
