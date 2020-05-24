c44d2f98eca7b82647402e04e05c6061b027f90d
bryan aviles <easyrun32@gmail.com>
2020-05-24 01:36:14 -0400

    testing

--- src/components-aboutus/paragraph1/paragraph1.component.jsx
+++ src/components-aboutus/paragraph1/paragraph1.component.jsx
@@ -4,29 +4,22 @@ import church from "../../images/aboutus/church.jpg";
 export const Paragraph1 = () => {
   return (
     <div className="paragraph1">
-      <div className="paragraph1__left-half">
-        <article className="paragraph1__article">
-          <div className="paragraph1__left-half__title">Where it began</div>
-          <p>
-            Frank Stevens began roofing in Central Valley, New York in 1960 at
-            the age of 18 for Jones Brothers, a roofing and plumbing company
-            started in the late 1800s. In 1973, he opened Frank Stevens Roofing
-            and went on to incorporate in 1999 as Frank Stevens & Sons Roofing,
-            Inc. We are family-owned and operated specializing in the
-            replacement, repair, restoration and maintenance of commercial,
-            industrial, institutional and residential roofing systems on
-            low-slope and steep slope roofs, including built-up hot tar systems,
-            single-ply membranes (EPDM Rubber, TPO & PVC), asphalt shingles,
-            real and imitation slate, cedar shake shingles and metal roofing
-            (standing seam panel and metal shingles).
-          </p>
-        </article>
+      <div className="paragraph1-leftside">
+        <div className="paragraph1-text">
+          Frank Stevens began roofing in Central Valley, New York in 1960 at the
+          age of 18 for Jones Brothers, a roofing and plumbing company started
+          in the late 1800s. In 1973, he opened Frank Stevens Roofing and went
+          on to incorporate in 1999 as Frank Stevens & Sons Roofing, Inc. We are
+          family-owned and operated specializing in the replacement, repair,
+          restoration and maintenance of commercial, industrial, institutional
+          and residential roofing systems on low-slope and steep slope roofs,
+          including built-up hot tar systems, single-ply membranes (EPDM Rubber,
+          TPO & PVC), asphalt shingles, real and imitation slate, cedar shake
+          shingles and metal roofing (standing seam panel and metal shingles).
+        </div>
       </div>
-
-      <div className="paragraph1__right-half">
-        <article className="paragraph1__article">
-          <img className="paragraph1__church" src={church} alt="church" />
-        </article>
+      <div className="paragraph1-rightside">
+        <div className="paragraph1-img"></div>
       </div>
     </div>
   );
